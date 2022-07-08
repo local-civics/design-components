902e74f03a0c871631cc32ffa44cc35ac0ebe782
Percee Goings <percee@pghq.app>
2022-07-07 20:09:45 -0700

    BUILD-16 Export activity reflection

--- src/components/Activity/index.ts
+++ src/components/Activity/index.ts
@@ -1,4 +1,5 @@
 export * from "./ActivityDetails/ActivityDetails";
 export * from "./ActivityItem/ActivityItem";
 export * from "./ActivityList/ActivityList";
+export * from "./ActivityReflection/ActivityReflection";
 export * from "./TagFilter/TagFilter";
