0f3111aacd389214525be5b8d81c43913eef7ac5
Percee Goings <percee@pghq.app>
2022-08-17 13:11:39 -0700

    ENG-347 Selected alpha testing issues

--- src/features/learning-forms/LearningForm/component.stories.tsx
+++ src/features/learning-forms/LearningForm/component.stories.tsx
@@ -6,9 +6,53 @@ import { Story } from "@storybook/react";
  * Storybook component configuration
  */
 export default {
-  title: "Learning Forms/LearningForm",
-  component: LearningForm,
+    title: "Learning Forms/LearningForm",
+    component: LearningForm,
 };
+
+const parksAndGreenForm: LearningFormProps = {
+    "formId": "b4a4dfd7-83e7-4bda-b24d-55648f3e703a",
+    "displayName": "Apply the Parks & Green Spaces Civic Lens ",
+    "description": "Civic Lenses are filters we can use to closely examine our community by focusing deeply on one particular area. Applying different Civic Lenses can help shift our perspective around community issues and reveal assets/resources and connections to other aspects of community that maybe we didn't notice before!\n\nIn this activity, you will apply the parks & green spaces civic lens to your local neighborhood and community. This means you will consider strengths and areas of improvement for access to recreation and nature for all people.\n",
+    "imageURL": "https://uploads-ssl.webflow.com/5ead22f7b41a94042ab113cd/623b30c8692b05e095358eae_civicMilestone_parks.png",
+    "eta": "15m",
+    "items": [
+        {
+            "itemId": "db038ce6-d961-44dc-8ad1-8ea5a352d60a",
+            "displayName": "Watch the following video on New York City's Central Park:",
+            "format": "embed",
+            "url": "https://www.youtube.com/embed/9GJD8bhFeaE"
+        },
+        {
+            "itemId": "a0d44064-57ef-4ae8-b97e-bdb55c02dd46",
+            "displayName": "Now use Google Maps to explore a new park near your home or school. You can click on the name for more information and even read reviews to learn more. In some parks, you can use Google Maps \"Street View\" to explore further. ",
+            "format": "image",
+            "url": "https://lh5.googleusercontent.com/_Iq88GX6PoEqCu3kBoQl5MFWVVuPtM_CxZdLNV5TQUbI51ciQdr2ZLl8O2sZXmr6BaaW5XQshGDx2gwMWuiaG0bdEw1SFsBlVhN-Mc5Y0b8VzQSpASzkjifROLpD_WOx5w"
+        },
+        {
+            "itemId": "f11f46ef-f98d-4031-8726-ff691c4560c3",
+            "displayName": "What types of assets/resources do you see in the park/green space you chose?",
+            "description": "Select all that apply!",
+            "format": "question",
+            "questionType": "checkbox",
+            "options": [
+                "Trees/bushes & other plants",
+                "Flowers",
+                "Open spaces with grass",
+                "Trash cans",
+                "Recycling bins",
+                "Sports fields",
+                "Walking/biking paths",
+                "Public restrooms",
+                "Animals/insects",
+                "Public Art (murals/sculptures)",
+                "Benches",
+                ""
+            ],
+            "required": true
+        }
+    ]
+}
 
 const publicSpeakingForm: LearningFormProps = {
     "formId": "5b0c2c0f-5f23-48ee-a0af-ed4f08ab23fc",
@@ -131,20 +175,38 @@ const publicSpeakingForm: LearningFormProps = {
 /**
  * Component storybook template
  */
-const Template: Story<LearningFormProps> = (args) => (
-  <div className="font-proxima m-auto">
-    <LearningForm
-      {...publicSpeakingForm}
-      {...args}
-      onSubmit={async (reflection, rating) => {
-        args.onSubmit && (await args.onSubmit(reflection, rating));
-      }}
-    />
-  </div>
-);
+const Template: Story<LearningFormProps> = (args) => {
+
+    const [state, setState] = React.useState({} as any)
+
+    return <div className="font-proxima m-auto">
+        <LearningForm
+            {...args}
+            items={args.items?.map(i => {
+                return {
+                    ...i,
+                    responses: state[i.itemId||""],
+                    onResponseChange: (responses) => {
+                        console.log({...state, [i.itemId||""]: responses})
+                        setState({...state, [i.itemId||""]: responses})
+                    }
+                }
+            })}
+            onSubmit={async (reflection, rating) => {
+                args.onSubmit && (await args.onSubmit(reflection, rating));
+            }}
+        />
+    </div>
+};
 
 /**
  * Component stories
  */
-export const Component: Story<LearningFormProps> = Template.bind({});
-Component.args = {};
+export const ParksAndGreenForm: Story<LearningFormProps> = Template.bind({});
+ParksAndGreenForm.args = {...parksAndGreenForm};
+
+/**
+ * Component stories
+ */
+export const PublicSpeakingForm: Story<LearningFormProps> = Template.bind({});
+PublicSpeakingForm.args = {...publicSpeakingForm};
