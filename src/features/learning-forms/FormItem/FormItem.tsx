863300d13cb4cd46ab3372ad1e31ef164bd06c8e
Percee Goings <percee@pghq.app>
2022-08-18 12:48:03 -0700

    ENG-347 Remove invalid state from choose file form question

--- src/features/learning-forms/FormItem/FormItem.tsx
+++ src/features/learning-forms/FormItem/FormItem.tsx
@@ -312,9 +312,7 @@ const FileUploadQuestion = (props: FormItemProps) => {
           placeholder="Paste a url or select a file above"
           className="w-full mt-1 block px-3 py-2 bg-white text-slate-500 focus:text-slate-600 border border-slate-300 rounded-sm text-sm shadow-sm placeholder-slate-400
           focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-          invalid:border-pink-500 invalid:text-pink-600
-          focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
         />
       </div>
     </div>
