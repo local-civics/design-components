5c727527d719ba7562aa978300a5b258f6ba553b
ravneek-kaur <75414646+ravneek-kaur@users.noreply.github.com>
2022-08-19 13:09:48 -0400

    email template in progress

--- src/components/EmailTemplate/EmailTemplate.tsx
+++ src/components/EmailTemplate/EmailTemplate.tsx
@@ -6,9 +6,7 @@ export type EmailTemplateProps = {
     imageURL?: string;
     videolink?: string;
     weblink?: string;
-    body1?: string;
-    body2?: string;
-    body3?: string;
+    body?: string;
 };
 
 /**
@@ -27,9 +25,7 @@ export const EmailTemplate = (props: EmailTemplateProps) => {
             </div>
             <div className="h-400 w-6/12 mx-auto space-y-3 font-sans text-gray-500 py-8">
                 <p className="text-left">Hi {props.name},</p>
-                <p className="text-left">{props.body1}</p>
-                <p className="text-left">{props.body2}</p>
-                <p className="text-left">{props.body3}</p>
+                <p className="text-left break-words block">{props.body}</p>
                 <div>
                     <p><iframe className="aspect-video" src={props.videolink}/></p>
                 </div>
@@ -54,7 +50,7 @@ export const EmailTemplate = (props: EmailTemplateProps) => {
                             className="w-4/12 h-4/12 mx-auto"
                             src={"https://ci3.googleusercontent.com/proxy/KkCqE6Cw12qEVuwvlR8-sliWEiVc51XbZQiQYovt25VZ9_xxz11bfymCILIiCmeLDAIb0R-lIWV0iWCNYUIfLi7AOeTQCz0Lh9QcFAe-_YVqgUowsT9XX_aKIcwHOWWtXf42pBo=s0-d-e1-ft#https://cdn-images.mailchimp.com/icons/social-block-v2/outline-dark-twitter-48.png"}
                             />
-                        </a>
+                        </a> 
                     </div>
                     <div>
                         <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__localcivics.us5.list-2Dmanage.com_track_click-3Fu-3Dadbb329311074b5cd6c7abbcf-26id-3D035581aecd-26e-3D4c221502e8&d=DwMFaQ&c=009klHSCxuh5AI1vNQzSO0KGjl4nbi2Q0M1QLJX9BeE&r=3IHjltefCtOlhcNxb9fORpTXY6gZe6GiGmNzCtcv7nc&m=SLjAejU1cXtuKCYjClySdGJQyRa0UYS8M9qiB6N_YihANEamdcApS3nDHqnSjElJ&s=hvZ7SqFq8EaJwpHdfDuw-Qulk-1Uj0ZYPej7yUwOK_w&e=">
@@ -77,7 +73,7 @@ export const EmailTemplate = (props: EmailTemplateProps) => {
                     <p className="text-center italic">Copyright © 2021 Local Impact Inc., All rights reserved.</p>
                     <p className="text-center text-primary-600"> .</p>
                     <p className="text-center">Want to change how you receive these emails?</p>
-                    <p className="text-center">You can <a href="hhttps://urldefense.proofpoint.com/v2/url?u=https-3A__localcivics.us5.list-2Dmanage.com_profile-3Fu-3Dadbb329311074b5cd6c7abbcf-26id-3D3ff118c070-26e-3D4c221502e8-26c-3D1d48f6db95&d=DwMFaQ&c=009klHSCxuh5AI1vNQzSO0KGjl4nbi2Q0M1QLJX9BeE&r=3IHjltefCtOlhcNxb9fORpTXY6gZe6GiGmNzCtcv7nc&m=SLjAejU1cXtuKCYjClySdGJQyRa0UYS8M9qiB6N_YihANEamdcApS3nDHqnSjElJ&s=oQvYmLMdwX7q_C5DLOsy9WsumkjXZsiHjuz7PCvo4sw&e=" className="text-white underline">update your preferences</a> or <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__localcivics.us5.list-2Dmanage.com_unsubscribe-3Fu-3Dadbb329311074b5cd6c7abbcf-26id-3D3ff118c070-26e-3D4c221502e8-26c-3D1d48f6db95&d=DwMFaQ&c=009klHSCxuh5AI1vNQzSO0KGjl4nbi2Q0M1QLJX9BeE&r=3IHjltefCtOlhcNxb9fORpTXY6gZe6GiGmNzCtcv7nc&m=SLjAejU1cXtuKCYjClySdGJQyRa0UYS8M9qiB6N_YihANEamdcApS3nDHqnSjElJ&s=NJ2mjdOr_9D3QO_RQNzOBw-KPdTZT4nFgDHaXma33_A&e=" className="text-white underline">unsubscribe from this list</a>.</p>
+                    <p className="text-center">You can <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__localcivics.us5.list-2Dmanage.com_profile-3Fu-3Dadbb329311074b5cd6c7abbcf-26id-3D3ff118c070-26e-3D4c221502e8-26c-3D1d48f6db95&d=DwMFaQ&c=009klHSCxuh5AI1vNQzSO0KGjl4nbi2Q0M1QLJX9BeE&r=3IHjltefCtOlhcNxb9fORpTXY6gZe6GiGmNzCtcv7nc&m=SLjAejU1cXtuKCYjClySdGJQyRa0UYS8M9qiB6N_YihANEamdcApS3nDHqnSjElJ&s=oQvYmLMdwX7q_C5DLOsy9WsumkjXZsiHjuz7PCvo4sw&e=" className="text-white underline">update your preferences</a> or <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__localcivics.us5.list-2Dmanage.com_unsubscribe-3Fu-3Dadbb329311074b5cd6c7abbcf-26id-3D3ff118c070-26e-3D4c221502e8-26c-3D1d48f6db95&d=DwMFaQ&c=009klHSCxuh5AI1vNQzSO0KGjl4nbi2Q0M1QLJX9BeE&r=3IHjltefCtOlhcNxb9fORpTXY6gZe6GiGmNzCtcv7nc&m=SLjAejU1cXtuKCYjClySdGJQyRa0UYS8M9qiB6N_YihANEamdcApS3nDHqnSjElJ&s=NJ2mjdOr_9D3QO_RQNzOBw-KPdTZT4nFgDHaXma33_A&e=" className="text-white underline">unsubscribe from this list</a>.</p>
                 </div>
             </div>
         </div>
