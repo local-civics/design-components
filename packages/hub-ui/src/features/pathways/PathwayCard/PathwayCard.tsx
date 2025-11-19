e0dce33f875b75d363231a37f76eae48150b5975
robchummel <robchummel@gmail.com>
2025-11-19 06:12:34 -0600

    Styled Badges in Pathway Preview

--- packages/hub-ui/src/features/pathways/PathwayCard/PathwayCard.tsx
+++ packages/hub-ui/src/features/pathways/PathwayCard/PathwayCard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { Icon, IconName } from "../../../components/Icon/Icon";
 import { Button } from "../../../components/Button/Button";
 import { Card } from "../../../components/Card/Card";
 import { BadgeEmblem } from "../../badges/BadgeEmblem/BadgeEmblem";
@@ -12,6 +12,7 @@ export type BadgeItem = {
   displayName: string;
   categories: string[];
   completedAt?: string | null;
+  weight: number;
   onClick?: () => void;
 };
 
@@ -47,70 +48,93 @@ export const PathwayCard = (props: PathwayCardProps) => {
     <Card onClose={props.onClose}>
       <div className="pb-5 text-zinc-600">
         <div className="pb-5 px-5 flex gap-x-2">
-        <BadgeEmblem
-            imageURL={props.imageURL}
-            alt={props.title}
-            size="sm"
+
+          <BadgeEmblem
+          iconURL={props.imageURL} //This is correct, the iconrl is populated in the BadgeEmblem for badges. level, imageUARL, and icon are undefined.
+          alt={props.title}
+          size="md"
           />
+
           <div className="max-w-[20rem]">
           <p className="font-semibold">{props.title}</p>
-            <div className="text-xs">
-            <span className="text-zinc-500 font-semibold">
-                This pathway is comprised of {target} Badges. It includes required and elective programming.
-              </span>
-              <span className="text-zinc-500 font-semibold">
-                Progress {progress}/{target}
-              </span>
-            </div>
-            {props.description && <p className="mt-2 text-sm">{props.description}</p>}
+          <div className="text-xs">
+            <span className="text-zinc-500 font-semibold">Tags</span>
+            {(props.displayTags ?? []).length > 0 && (
+              <div className="inline-block ml-1 text-green-500">
+                {(props.displayTags ?? []).map((tag, index) => (
+                  <span key={index} className="font-semibold mr-1">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              )}
+
+          </div>
+            {!!props.description && <p className="mt-2 text-sm">{props.description}</p>}
           </div>
         </div>
-        {props.displayTags && props.displayTags.length > 0 && (
-          <ul className="flex flex-wrap gap-2 px-5 mt-2">
-            {props.displayTags.map((tag, idx) => (
-              <li
-                key={idx}
-                className="px-2 py-1 bg-gray-200 rounded text-sm font-medium"
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        )}
-
-        {badges.length > 0 && (
         <div className="p-5 grid grid-cols-1 gap-y-3 md:min-w-[30rem] max-w-[40rem] border-t border-zinc-200">
-          <p className="font-semibold">Badges</p>
+          <p className="font-semibold">Pathway Badges & Criteria</p>
+          <p className="text-xs">This pathway is comprised of {target} Badges. It includes required and elective programming.</p>
+          <p className="text-xs">Progress: {progress} / {target} badges completed.</p>
+
           <div className="mt-2 grid grid-cols-1 gap-y-2 max-h-[18rem] overflow-y-auto">
-            {badges.map((b) => (
-          <div
-            key={b.badgeId}
-            className="cursor-pointer hover:underline text-blue-600"
-            onClick={b.onClick}
-          >
-            {b.displayName} {b.completedAt ? "(Completed)" : ""}
+            {badges.map((b) => {
+              const buttonText = b.completedAt ? "Completed" : "Start";//TODO get startedAT b.startedAt ? "Continue" : "Start";
+              const buttonTheme = b.completedAt ? "light" : "dark";
+              const iconName = b.completedAt ? "check & circle" : "circle";
+              const iconColor = b.completedAt ? "text-green-500" : "text-zinc-300";
+
+              return (
+                  <div key={b.badgeId} className="flex gap-x-2 items-center">
+                    <div className={`shrink-0 h-4 w-4 ${iconColor}`}>
+                      <Icon name={iconName} />
+                    </div>
+                    <div className="grow">
+                      <div>
+                        <span className="font-semibold">{b.displayName}</span>
+                      </div>
+                      <div className="text-xs">
+                        <span>{b.displayName}</span>
+                        {<span className="ml-1 text-green-500 font-semibold whitespace-nowrap">
+                          {b.weight} {b.weight === 1 ? "point" : "points"}
+                        </span>}
+                      </div>
+                    </div>
+                    { <div className="shrink-0 w-full max-w-[7rem]">
+                      <Button
+                        onClick={b.onClick}
+                        spacing="sm"
+                        border="rounded"
+                        size="full:sm"
+                        color="blue"
+                        theme={buttonTheme}
+                        text={buttonText}
+                      />
+                    </div> }
+                  </div>
+              );
+            })}
+      
           </div>
-          ))}
-      </div>
-    </div>
-  )}
-  { props.onSubmit &&
-            <div className="pt-5 px-5 flex border-t border-zinc-200">
-              <div className="w-full max-w-[7rem] ml-auto">
-                <Button
-                    disabled={true}
-                    onClick={props.onSubmit}
-                    spacing="sm"
-                    border="rounded"
-                    size="full:sm"
-                    color="blue"
-                    theme="dark"
-                    text="Submit"
-                />
-              </div>
+
+
+        </div>
+          <div className="pt-5 px-5 flex border-t border-zinc-200">
+            <div className="w-full max-w-[7rem] ml-auto">
+              <Button
+                  disabled={true} //TODO: enable Pathway Submission
+                  onClick={props.onSubmit}
+                  spacing="sm"
+                  border="rounded"
+                  size="full:sm"
+                  color="blue"
+                  theme="dark"
+                  text="Submit"
+              />
             </div>
-        }
-</div>
-</Card>
-);
+          </div>
+      </div>
+      </Card>
+  );
 };
