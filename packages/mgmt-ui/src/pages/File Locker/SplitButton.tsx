2f10d26ed9eba737f2c7f359cf89f50c7f963b43
robchummel <robchummel@gmail.com>
2026-04-01 02:55:29 -0500

    Badge Category Toggle

---
+++ packages/mgmt-ui/src/pages/File Locker/SplitButton.tsx
@@ -0,0 +1,58 @@
+import * as React                                        from 'react'
+import { createStyles, Button, Stack, Menu, Group, ActionIcon } from '@mantine/core';
+import {
+    IconChevronDown,
+    IconPlaylistAdd,
+    IconClipboardCopy, IconTableExport
+}                                                        from '@tabler/icons';
+import {Link}                                            from "react-router-dom";
+
+const useStyles = createStyles((theme) => ({
+    button: {
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+    },
+
+    menuControl: {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        border: 0,
+        borderLeft: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white}`,
+    },
+}));
+
+export type SplitButtonProps = {
+    href: string
+    onCopyLinkClick: () => void;
+    onExportDataClick: () => void;
+}
+
+export const SplitButton = (props: SplitButtonProps) => {
+    const { classes, theme } = useStyles();
+    const menuIconColor = theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 6];
+
+    return (
+        <Stack spacing="sm">
+            <Button<typeof Link> component={Link} to={props.href}
+                className={classes.button}
+                variant="gradient"
+            >
+                Preview
+            </Button>
+            <Button
+                leftIcon={<IconClipboardCopy size={14} />}
+                onClick={props.onCopyLinkClick}
+            >
+                Copy link
+            </Button>
+            <Button
+                leftIcon={<IconTableExport size={14} />}
+                onClick={props.onExportDataClick}
+            >
+                Export data (.csv)
+            </Button>
+        </Stack>
+    );
+}
---
+++ packages/mgmt-ui/src/pages/File Locker/Table.tsx
@@ -0,0 +1,118 @@
+import {DataTable}                                from "mantine-datatable";
+import * as React                                 from 'react';
+import {Avatar, Group, Text, ScrollArea, Badge}   from '@mantine/core';
+import {Link}                                     from "react-router-dom";
+import {
+    PlaceholderBanner
+}                                                 from "../../components/banners/PlaceholderBanner/PlaceholderBanner";
+import {Stack as BadgeStack, Item as BadgeItem} from "./BadgeStack";
+
+/**
+ * Item
+ */
+export interface Item {
+    userId: string
+    avatar: string
+    name: string
+    email: string
+    isComplete?: boolean
+    badges: BadgeItem[]
+    categoryPoints?: Record<string, number>
+}
+
+/**
+ * TableData
+ */
+export type TableData = {
+    loading: boolean
+    items: Item[]
+}
+/**
+ * Category
+ */
+export type Category = {
+    categoryId: string
+    name: string
+}
+
+/**
+ * TableProps
+ */
+export type TableProps = TableData & {
+    categories: Category[]
+}
+
+/**
+ * Table
+ * @constructor
+ * @param props
+ */
+export function Table(props: TableProps) {
+    if(props.items.length === 0){
+        return <PlaceholderBanner
+            title="No badges to display"
+            description="There has not been any badge progress just yet."
+            loading={props.loading}
+            icon="badges"
+        />
+    }
+
+    return (
+        <ScrollArea.Autosize maxHeight={600}>
+            <DataTable
+                verticalSpacing="sm"
+                sx={{ minWidth: 700 }}
+                withBorder={false}
+                borderRadius="sm"
+                withColumnBorders
+                striped
+                highlightOnHover
+                records={props.items}
+                idAccessor="userId"
+                columns={[{
+                    accessor: 'name',
+                    title: 'Student Name',
+                    render: (row: Item) => (
+                        <Group spacing="sm">
+                            <Avatar size={40} src={row.avatar} radius={40}/>
+                            <div>
+                                <Text size="sm" weight={500}>
+                                    {row.name}
+                                </Text>
+                                <Text size="xs" color="dimmed">
+                                    {row.email}
+                                </Text>
+                            </div>
+                        </Group>
+                    ),
+                },{
+                    accessor: 'status',
+                    render: (row: Item) => (
+                        <>
+                            {!!row.isComplete && <Badge variant="filled">Complete</Badge>}
+                            {!row.isComplete && <Badge color="red" variant="filled">Incomplete</Badge>}
+                        </>
+                    )
+
+                },
+                ...props.categories.map((category) => ({
+                    accessor: category.categoryId,
+                    title: category.name,
+                    render: (row: Item) => (
+                        <Badge color="blue" variant="filled">
+                            {row.categoryPoints?.[category.categoryId] ?? 0}
+                        </Badge>
+                    )
+                }))
+            ]}
+
+
+                rowExpansion={{
+                    content: ({ record }: {record: Item}) => (
+                        <BadgeStack items={record.badges}/>
+                    ),
+                }}
+            />
+        </ScrollArea.Autosize>
+    );
+}
