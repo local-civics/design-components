6227eaa5d9ffb3e28fba5e63a5be7f1e21f280e4
Cleon0 <69090561+Cleon0@users.noreply.github.com>
2026-04-10 15:52:03 -0400

    Replacing mantine-table with DataTable import

--- packages/mgmt-ui/src/pages/Class/Table.tsx
+++ packages/mgmt-ui/src/pages/Class/Table.tsx
@@ -1,42 +1,20 @@
-import {openConfirmModal} from "@mantine/modals";
-import * as React from         'react';
+import {openConfirmModal}                                           from "@mantine/modals";
+import * as React                                                   from 'react';
+import { DataTable, DataTableSortStatus }                           from 'mantine-datatable'
 import {
     Avatar,
-    Table as MantineTable,
     Group,
     Text,
     ActionIcon,
-    UnstyledButton,
     ScrollArea,
     Select,
-    Box,
-    Center,
-    createStyles
-}                              from '@mantine/core';
-import {
-    IconCheck, 
-    IconTrash,
-    IconSelector,
-    IconChevronDown,
-    IconChevronUp
-}                              from '@tabler/icons';
-import {Link}                  from "react-router-dom";
-import {
-    PlaceholderBanner
-}                              from "../../components/banners/PlaceholderBanner/PlaceholderBanner";
-import {relativeTimeFromDates} from "../../utils/time";
-import {useSortableData}       from "../../utils/useSortableData";
-
-const useStyles = createStyles((theme) => ({
-    th: { padding: '0 !important' },
-    control: {
-        width: '100%',
-        padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-        '&:hover': {
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-    },
-}));
+    Badge,
+}                                                                   from '@mantine/core';
+import { IconCheck, IconTrash, IconX }                              from '@tabler/icons';
+import {Link}                                                       from "react-router-dom";
+import { PlaceholderBanner }                                        from "../../components/banners/PlaceholderBanner/PlaceholderBanner";
+import {relativeTimeFromDates}                                      from "../../utils/time";
+import {useSortableData}                                            from "../../utils/useSortableData";
 
 /**
  * Item
@@ -58,32 +36,19 @@ export type Item = {
 }
 
 /**
- * TableProps
+ * TableData
  */
-export interface TableProps {
+export type TableData = {
     loading: boolean
-    items: Item[];
-
-    onDelete?: (member: Item) => void
-    onRoleChange?: (user: Item, role: string | null) => void;
+    items: Item[]
 }
 
 /**
- * Th Helper for Sortable Headers
+ * TableProps
  */
-function Th({ children, reversed, sorted, onSort }: { children: React.ReactNode, reversed: boolean, sorted: boolean, onSort(): void }) {
-    const { classes } = useStyles();
-    const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
-    return (
-        <th className={classes.th}>
-            <UnstyledButton onClick={onSort} className={classes.control}>
-                <Group position="apart" noWrap spacing="xs">
-                    <Text weight={500} size="sm" sx={{ whiteSpace: 'nowrap' as const }}>{children}</Text>
-                    <Center><Icon size={14} stroke={1.5} /></Center>
-                </Group>
-            </UnstyledButton>
-        </th>
-    );
+export type TableProps = TableData & {
+    onDelete: (member: Item) => void
+    onRoleChange: (user: Item, role: string | null) => void;
 }
 
 /**
@@ -103,6 +68,11 @@ export function Table(props: TableProps) {
             icon="groups"
         />
     }
+    
+    const sortStatus: DataTableSortStatus = {
+        columnAccessor: sortConfig.key as string,
+        direction: sortConfig.direction === 'desc' ? 'desc' : 'asc',
+    };
 
     const openDeleteModal = (student: Item) => openConfirmModal({
         title: `Remove "${student.givenName && student.familyName ? `${student.givenName} ${student.familyName}` : student.email}" from this class?`,
@@ -116,95 +86,96 @@ export function Table(props: TableProps) {
         confirmProps: { color: 'red' },
         onConfirm: () => props.onDelete && props.onDelete(student),
     });
-
-    const rows = sortedItems.map((row) => (
-        <tr key={row.email}>
-            <td>
-                <UnstyledButton<typeof Link> component={Link} to={row.href}>
-                    <Group spacing="sm">
-                        <Avatar size={40} src={row.avatar} radius={40} />
-                        <div>
-                            <Text size="sm" weight={500}>
-                                {row.givenName && row.familyName ? `${row.givenName} ${row.familyName}` : row.email}
-                            </Text>
-                            <Text size="xs" color="dimmed">
-                                {row.email}
-                            </Text>
-                        </div>
-                    </Group>
-                </UnstyledButton>
-            </td>
-            <td>
-                <Box maw={150}>
-                    <Select
-                        size="sm"
-                        value={row.isAdmin ? "educator" : "student"}
-                        onChange={(value) => props.onRoleChange && props.onRoleChange(row, value)}
-                        data={[{value: "student", label: "Student"}, {value: "educator", label: "Educator"}]}
-                    />
-                </Box>
-            </td>
-            <td>{row.badgesEarned}</td>
-            <td>{row.lessonsCompleted}</td>
-            <td>{row.hasAccount && <IconCheck color="green" />}</td>
-            <td>{row.lastActivity ? relativeTimeFromDates(row.lastActivity) : ""}</td>
-            <td>
-                <Group noWrap spacing={0} position="right">
-                    { !row.readonly && !!props.onDelete && <ActionIcon color="red">
-                        <IconTrash onClick={() => openDeleteModal(row)} size={16} stroke={1.5} />
-                    </ActionIcon> }
-                </Group>
-            </td>
-        </tr>
-    ));
-
+    
     return (
         <ScrollArea>
-            <MantineTable verticalSpacing={20} sx={{ minWidth: 700 }} highlightOnHover striped>
-                <thead>
-                    <tr>
-                        {/*Link headers to sorting keys */}
-                        <Th 
-                            sorted={sortConfig.key === 'givenName'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('givenName')}
-                        >Name</Th>
-                        
-                        <Th 
-                            sorted={sortConfig.key === 'isAdmin'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('isAdmin')}
-                        >Role</Th>
-                        
-                        <Th 
-                            sorted={sortConfig.key === 'badgesEarned'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('badgesEarned')}
-                        >Badges Earned</Th>
-                        
-                        <Th 
-                            sorted={sortConfig.key === 'lessonsCompleted'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('lessonsCompleted')}
-                        >Lessons Completed</Th>
-                        
-                        <Th 
-                            sorted={sortConfig.key === 'hasAccount'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('hasAccount')}
-                        >Account Created?</Th>
-                        
-                        <Th 
-                            sorted={sortConfig.key === 'lastActivity'} 
-                            reversed={sortConfig.direction === 'desc'} 
-                            onSort={() => requestSort('lastActivity')}
-                        >Last Active</Th>
-                        
-                        <th></th> {/* Actions column (Trash can) */}
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </MantineTable>
+            <DataTable
+                verticalSpacing={20}
+                sx={{ minWidth: 700 }}
+                highlightOnHover
+                striped
+                records={sortedItems}
+                idAccessor="email"
+                sortStatus={sortStatus}
+                onSortStatusChange={(status) => requestSort(status.columnAccessor)}
+                columns={[
+                    {
+                        accessor: 'givenName',
+                        title: 'Name',
+                        sortable: true,
+                        render: (row: Item) => (
+                            <UnstyledButton<typeof Link> component={Link} to={row.href}>
+                                <Group spacing="sm">
+                                    <Avatar size={40} src={row.avatar} radius={40} />
+                                    <div>
+                                        <Text size="sm" weight={500}>
+                                            {row.givenName && row.familyName ? `${row.givenName} ${row.familyName}` : row.email}
+                                        </Text>
+                                        <Text size="xs" color="dimmed">
+                                            {row.email}
+                                        </Text>
+                                    </div>
+                                </Group>
+                            </UnstyledButton>
+                        ),
+                    },
+                    {
+                        accessor: 'isAdmin',
+                        title: 'Role',
+                        sortable: true,
+                        render: (row: Item) => (
+                            <Box maw={150}>
+                                <Select
+                                    size="sm"
+                                    disabled={row.readonly}
+                                    value={row.isAdmin ? "educator" : "student"}
+                                    onChange={(value) => props.onRoleChange && props.onRoleChange(row, value)}
+                                    data={[
+                                        { label: 'Student', value: 'student' },
+                                        { label: 'Educator', value: 'educator' },
+                                    ]}
+                                />
+                            </Box>
+                        )
+                    },
+                    {
+                        accessor: 'badgesEarned',
+                        title: 'Badges Earned',
+                        sortable: true,
+                    },
+                    {
+                        accessor: 'lessonsCompleted',
+                        title: 'Lessons Completed',
+                        sortable: true,
+                    },
+                    {
+                        accessor: 'hasAccount',
+                        title: 'Account Created?',
+                        sortable: true,
+                        render: (row: Item) => row.hasAccount ? <IconCheck color="green" /> : null
+                    },
+                    {
+                        accessor: 'lastActivity',
+                        title: 'Last Active',
+                        sortable: true,
+                        render: (row: Item) => row.lastActivity ? relativeTimeFromDates(row.lastActivity) : ""
+                    },
+                    {
+                        accessor: 'actions',
+                        title: '',
+                        textAlign: 'right',
+                        render: (row: Item) => (
+                            <Group noWrap spacing={0} position="right">
+                                { !row.readonly && !!props.onDelete && (
+                                    <ActionIcon color="red" onClick={() => openDeleteModal(row)}>
+                                        <IconTrash size={16} stroke={1.5} />
+                                    </ActionIcon>
+                                )}
+                            </Group>
+                        )
+                    }
+                ]}
+            />
         </ScrollArea>
     );
 }
--- packages/mgmt-ui/src/pages/Pathway/BadgeTable.tsx
+++ packages/mgmt-ui/src/pages/Pathway/BadgeTable.tsx
@@ -1,26 +1,12 @@
 import * as React                                                                           from 'react';
-import {Table as MantineTable, ScrollArea, Text, UnstyledButton, Group, Center, createStyles} from '@mantine/core';
-import {Link}                                                                               from "react-router-dom";
-import {
-    PlaceholderBanner
-}                                                                                           from "../../components/banners/PlaceholderBanner/PlaceholderBanner";
-import {IconSelector, IconChevronDown, IconChevronUp}                                       from '@tabler/icons';
+import { ScrollArea, Text }                                                                 from '@mantine/core';
+import { DataTable, DataTableSortStatus }                                                   from 'mantine-datatable';
+import { Link }                                                                               from "react-router-dom";
+import { PlaceholderBanner }                                                                                           from "../../components/banners/PlaceholderBanner/PlaceholderBanner";
 import { useSortableData }                                                                  from "../../utils/useSortableData";
 
-const useStyles = createStyles((theme) => ({
-    th: { padding: '0 !important' },
-    control: {
-        width: '100%',
-        padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-        '&:hover': {
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-    },
-}));
-
-
 /**
- * Item
+ * Item 
  */
 export interface Item {
     badgeId: string
@@ -42,23 +28,6 @@ export type TableData = {
  */
 export type TableProps = TableData
 
-function Th({ children, reversed, sorted, onSort }: { children: React.ReactNode, reversed: boolean, sorted: boolean, onSort(): void }) {
-    const { classes } = useStyles();
-    const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
-    return (
-        <th className={classes.th}>
-            <UnstyledButton onClick={onSort} className={classes.control}>
-                <Group position="apart" noWrap spacing="xs"> 
-                    <Text weight={500} size="sm" sx={{ whiteSpace: 'nowrap' as const }}>
-                        {children}
-                    </Text>
-                    <Center><Icon size={14} stroke={1.5} /></Center>
-                </Group>
-            </UnstyledButton>
-        </th>
-    );
-}
-
 /**
  * Table
  * @constructor
@@ -76,37 +45,44 @@ export function Table(props: TableProps) {
         />
     }
 
-    const rows = sortedItems.map((row: Item) => {
-        const percentageCompletion = Math.round((row.percentageCompletion + Number.EPSILON) * 100)
-        return <tr key={row.badgeId}>
-            <td>
-                <Text<typeof Link> component={Link} to={row.href}>
-                    {row.badgeName}
-                </Text>
-            </td>
-            <td>{percentageCompletion}%</td>
-        </tr>
-    });
+    const sortStatus: DataTableSortStatus = {
+        columnAccessor: sortConfig.key as string,
+        direction: sortConfig.direction === 'desc' ? 'desc' : 'asc',
+    };
 
     return (
         <ScrollArea.Autosize maxHeight={600}>
-            <MantineTable verticalSpacing="sm" sx={{ minWidth: 700 }} highlightOnHover striped>
-                <thead>
-                <tr>
-                    <Th 
-                        sorted={sortConfig.key === 'badgeName'} 
-                        reversed={sortConfig.direction === 'desc'} 
-                        onSort={() => requestSort('badgeName')}
-                    >Badge Name</Th>
-                    <Th 
-                        sorted={sortConfig.key === 'percentageCompletion'} 
-                        reversed={sortConfig.direction === 'desc'} 
-                        onSort={() => requestSort('percentageCompletion')}
-                    >Badge Completion</Th>
-                </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </MantineTable>
+            <DataTable
+                verticalSpacing="sm"
+                sx={{ minWidth: 700 }}
+                highlightOnHover
+                striped
+                records={sortedItems}
+                idAccessor="badgeId"
+                sortStatus={sortStatus}
+                onSortStatusChange={(status) => requestSort(status.columnAccessor)}
+                columns={[
+                    {
+                        accessor: 'badgeName',
+                        title: 'Badge Name',
+                        sortable: true,
+                        render: (row) => (
+                            <Text<typeof Link> component={Link} to={row.href} color="blue">
+                                {row.badgeName}
+                            </Text>
+                        ),
+                    },
+                    {
+                        accessor: 'percentageCompletion',
+                        title: 'Badge Completion',
+                        sortable: true,
+                        render: (row) => {
+                            const roundedValue = Math.round((row.percentageCompletion + Number.EPSILON) * 100);
+                            return `${roundedValue}%`;
+                        },
+                    },
+                ]}
+            />
         </ScrollArea.Autosize>
     );
 }
